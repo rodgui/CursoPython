8afb558c6aade49c4b301d4d6ba43649e0f27e4e
rodgui <rod.gui@gmail.com>
2021-12-08 18:21:49 -0300

    commit

--- Curso DevMedia/main.py
+++ Curso DevMedia/main.py
@@ -7,8 +7,8 @@ print(a.getCPF())
 print(a.getNome())
 print(a.getIdade())
 
-#b = PessoaJuridica('64.614.527/0001-99', nome='Empresa X', idade=22)
+b = PessoaJuridica('64.614.527/0001-99', nome='Empresa X', idade=22)
 
-#print(b.getCNPJ())
-#print(b.getNome())
-#print(b.getIdade())
+print(b.getCNPJ())
+print(b.getNome())
+print(b.getIdade())
